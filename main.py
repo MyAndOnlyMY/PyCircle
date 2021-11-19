bda5498f79fea52dc26215a8858d3d7d0567a04e
IVN.21 <vikuha.ivanova@gamil.com>
2021-11-19 11:59:35 +0300

    Version 1.2 The circles which radius bigger 200 became blue circles with radius 20

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import turtle
 
 listA = list(map(int, input().split()))
+radius_ = 20
 for i in listA:
-    if i==max(listA):
-        turtle.color("red")
+    if i > 200:
+        turtle.color("blue")
+        turtle.fillcolor("blue")
+        turtle.begin_fill()
+        turtle.circle(radius_)
+        turtle.end_fill()
     else:
         turtle.color("black")
-    turtle.circle(i)
+        turtle.circle(i)
 turtle.done()
