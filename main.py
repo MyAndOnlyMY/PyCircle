81bbcb80b0708a97e07efd9519d523c0ad0c97a2
IVN.21 <vikuha.ivanova@gamil.com>
2021-11-19 11:47:52 +0300

    Version 1.1 The biggest circle is red

--- main.py
+++ main.py
@@ -2,5 +2,9 @@ import turtle
 
 listA = list(map(int, input().split()))
 for i in listA:
+    if i==max(listA):
+        turtle.color("red")
+    else:
+        turtle.color("black")
     turtle.circle(i)
 turtle.done()
